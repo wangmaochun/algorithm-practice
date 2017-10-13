0fe7a1c8a9fec5b25344803cddfc211c2eba1856
泡泡 <2631520922@qq.com>
2017-10-13 11:16:33 +0800

    Update chicken_rabbit.c

--- chicken_rabbit.c
+++ chicken_rabbit.c
@@ -19,3 +19,29 @@ int main()
 
 	return  0;
 }
+
+---------------------------
+method2:
+#include <stdio.h> 
+#include <math.h>
+int main()
+{
+	int n,m;
+	int x,y;
+	scanf("%d %d",&n,&m);
+	x = 2*n-m/2;
+	y = m -x ;
+	//腿数为偶数，兔和鸡个数非负 
+	//利用短路算法 
+	if (m%2==1 || x < 0 || y < 0)
+	{
+		printf("No answer\n"); //换行符 
+	}else
+	{
+		
+		printf("%d %d\n",x,y);
+	}
+	
+
+	return  0;
+}
